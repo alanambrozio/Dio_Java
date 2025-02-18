96d9d91cd187b45c372256898a321c914e698d95
alanca <ambrozioalan@gmail.com>
2025-02-18 16:48:56 -0300

    Subindo projeto Dio conta cliente

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        int numero = 5;
+        numero = -numero;
+        System.out.println(numero);
+        numero = -numero;
+        System.out.println(numero);
+
+
         System.out.println("Hello, World!");
     }
 }
